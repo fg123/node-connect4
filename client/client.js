c9e1a85a430710f3c026e44035e6b0e8133a652a
Michael Kim <micklepickle98@gmail.com>
2017-07-15 23:58:28 -0700

    Update code

--- client/client.js
+++ client/client.js
@@ -24,136 +24,141 @@ var player = 0;
 // to start connection with server:
 
 $(document).ready(function() {
-  canvas = $("#canvas")[0];
-	canvas.width = (gameWidth);
-	canvas.height = (gameHeight);
-  
-  // Init Squares
-  drawBoard();
-	
-  // Setup connection
-  setupSocket();
-	socket = io();
-  socket.emit("start");
+    canvas = $("#canvas")[0];
+    canvas.width = (gameWidth);
+    canvas.height = (gameHeight);
 
-	$(".gameWrapper").mousemove(function (event) {
-		if (isCurrentTurn) {
-			var leftOffset = $(".gameCanvas").offset().left;
-			var mouse = event.pageX - leftOffset;
-			var index = parseInt((event.pageX - leftOffset) / boxSize);
-			for (var i = 0; i < gridWidth; i++) {
-				if (index != i) {
-					$("#u" + i + " canvas").hide();
-				}
-				else {
-					$("#u" + i + " canvas").show();
-				}
-			}
-		}
-  });
-  
+    // Init Squares
+    drawBoard();
+
+    // Setup connection
+    setupSocket();
+
+    $(".gameWrapper").mousemove(function (event) {
+        if (isCurrentTurn) {
+            var leftOffset = $(".gameCanvas").offset().left;
+            var mouse = event.pageX - leftOffset;
+            var index = parseInt((event.pageX - leftOffset) / boxSize);
+            for (var i = 0; i < gridWidth; i++) {
+                if (index != i) {
+                    $("#u" + i + " canvas").hide();
+                }
+                else {
+                    $("#u" + i + " canvas").show();
+                }
+            }
+        }
+    });
+
 });
 
 function drawBoard() {
-	var ctx = canvas.getContext("2d");
-	ctx.fillStyle = "#FFF";
-	
-	ctx.lineWidth = 2;
-  ctx.fillRect(0, 0, gameWidth, gameHeight);
-  ctx.beginPath();
-  var leftX = (gameWidth - (gridWidth * boxSize)) / 2;
-  var padding = 20;
-  // Draw Boxes
-  for (var i = 0; i < gridWidth + 1; i++) {
-  	// Verticals
-    ctx.moveTo(leftX + (i * boxSize), gameHeight - padding);
-    ctx.lineTo(leftX + (i * boxSize), gameHeight - (gridHeight * boxSize) - padding);
-  }
-  for (var i = 0; i < gridHeight + 1; i++) {
-  	// Horizontals
-    ctx.moveTo(leftX, gameHeight - (i * boxSize) - padding);
-    ctx.lineTo(gameWidth - leftX, gameHeight - (i * boxSize) - padding);
-  }
-  ctx.strokeStyle = "black";
-  ctx.stroke();
-  // Draw the Circles
-  for (var col = 0; col < boardState.length; col++) {
-  	for (var row = 0; row < boardState[col].length; row++) {
-    	ctx.beginPath();
-			ctx.arc(leftX + ((col + 0.5) * boxSize), gameHeight - ((row + 0.5) * boxSize) - padding, pieceSize / 2, 0, 2 * Math.PI);
-      if (boardState[col][row] == 1) {
-      	ctx.fillStyle = "red";
-      }
-      else {
-      	ctx.fillStyle = "yellow";
-      }
-		ctx.fill();
+    var ctx = canvas.getContext("2d");
+    ctx.fillStyle = "#FFF";
+
+    ctx.lineWidth = 2;
+    ctx.fillRect(0, 0, gameWidth, gameHeight);
+    ctx.beginPath();
+    var leftX = (gameWidth - (gridWidth * boxSize)) / 2;
+    var padding = 20;
+    // Draw Boxes
+    for (var i = 0; i < gridWidth + 1; i++) {
+        // Verticals
+        ctx.moveTo(leftX + (i * boxSize), gameHeight - padding);
+        ctx.lineTo(leftX + (i * boxSize), gameHeight - (gridHeight * boxSize) - padding);
     }
-  }
+    for (var i = 0; i < gridHeight + 1; i++) {
+        // Horizontals
+        ctx.moveTo(leftX, gameHeight - (i * boxSize) - padding);
+        ctx.lineTo(gameWidth - leftX, gameHeight - (i * boxSize) - padding);
+    }
+    ctx.strokeStyle = "black";
+    ctx.stroke();
+    // Draw the Circles
+    for (var col = 0; col < boardState.length; col++) {
+        for (var row = 0; row < boardState[col].length; row++) {
+            ctx.beginPath();
+            ctx.arc(leftX + ((col + 0.5) * boxSize), gameHeight - ((row + 0.5) * boxSize) - padding, pieceSize / 2, 0, 2 * Math.PI);
+            if (boardState[col][row] == 1) {
+                ctx.fillStyle = "red";
+            }
+            else {
+                ctx.fillStyle = "yellow";
+            }
+            ctx.fill();
+        }
+    }
 }
 
 function initUserControl() {
-	// Init User Control after Sockets
-  for (var i = 0; i < gridWidth; i++) { 
-    var newDiv = $("<div id='u" + i + "'><canvas height='" + pieceSize + "' width='" + pieceSize + "'></canvas></div>");
-    newDiv.height(boxSize);
-    newDiv.width(boxSize);
-    newDiv.click(function () { 
-			handlePushPiece($(this).attr("id"));
-		});
-    $(".userControl").append(newDiv);
-    var ctx = newDiv.find("canvas")[0].getContext("2d");
-    ctx.fillStyle = "#FFF";
-    ctx.fillRect(0, 0, pieceSize, pieceSize);
-    ctx.beginPath();
-    ctx.arc(pieceSize / 2, pieceSize / 2, pieceSize / 2, 0, 2 * Math.PI);
-    if (player == 1) {
-          ctx.fillStyle = "red";
+    console.log('user');
+    // Init User Control after Sockets
+    for (var i = 0; i < gridWidth; i++) {
+        var newDiv = $("<div id='u" + i + "'><canvas height='" + pieceSize + "' width='" + pieceSize + "'></canvas></div>");
+        newDiv.height(boxSize);
+        newDiv.width(boxSize);
+        newDiv.click(function () {
+            handlePushPiece($(this).attr("id"));
+        });
+        $(".userControl").append(newDiv);
+        var ctx = newDiv.find("canvas")[0].getContext("2d");
+        ctx.fillStyle = "#FFF";
+        ctx.fillRect(0, 0, pieceSize, pieceSize);
+        ctx.beginPath();
+        ctx.arc(pieceSize / 2, pieceSize / 2, pieceSize / 2, 0, 2 * Math.PI);
+        if (player == 1) {
+            ctx.fillStyle = "red";
+        }
+        else {
+            ctx.fillStyle = "yellow";
+        }
+        ctx.fill();
     }
-    else {
-      ctx.fillStyle = "yellow";
-    }
-    ctx.fill();
-  }
 }
 
 function handlePushPiece(id) {
-	if (isCurrentTurn) {
-  	socket.emit("placePiece", parseInt(id.replace(/\D/g,'')));
-    isCurrentTurn = false;
-  }
+    console.log('click', isCurrentTurn);
+    if (isCurrentTurn) {
+        socket.emit("placePiece", parseInt(id.replace(/\D/g,'')));
+        isCurrentTurn = false;
+    }
 }
 
-/*
-socket.emit("placePiece", colNum);
-call to place piece on column colNum
-*/
 function setupSocket() {
-  if(socket) {
-    socket = io();
-  }
-  
-  socket.on("connected", function(role) {
-		if(role === 1) {
-      player = role;
+    if(!socket) {
+        socket = io();
     }
-    else if(role === 2) {
-      player = role;
-    }
-    else {
-      player = -1;
-    }
-    initUserControl();
-  });
-  
-  socket.on("newBoard", function(board) {
-    drawBoard();
-  });
-  
-  socket.on("winner", function(player) {
-  });
-  
-  socket.on("errorMsg", function(msg) {
-    console.log(msg);
-  });
+
+    socket.on("connected", function(role) {
+        if(role === 1) {
+            player = role;
+        }
+        else if(role === 2) {
+            player = role;
+        }
+        else {
+            player = -1;
+        }
+        initUserControl();
+    });
+
+    socket.on("newBoard", function(board) {
+        console.log('new board');
+        boardState = board;
+        drawBoard();
+    });
+
+    socket.on("turn", function() {
+        console.log("my turn");
+        isCurrentTurn = true;
+    })
+
+    socket.on("winner", function(player) {
+    });
+
+    socket.on("errorMsg", function(msg) {
+        console.log(msg);
+    });
+
+    socket.emit("start");
 }
--- server.js
+++ server.js
@@ -13,83 +13,110 @@ var maxHeight = 6;
 var currTurn = 0;
 
 function checkWin() {
-  // Columns
-  for(var i = 0; i < boardState.length; ++i) {
-    var colStr = boardState[i].toString();
-    if(colStr.indexOf("1,1,1,1") > -1) {
-      socket.emit("winner", 1);
+    // Columns
+    for(var i = 0; i < boardState.length; ++i) {
+        var colStr = boardState[i].toString();
+        if(colStr.indexOf("1,1,1,1") > -1) {
+            return 1;
+        }
+        else if(colStr.indexOf("2,2,2,2") > -1) {
+            return 2;
+        }
     }
-    else if(colStr.indexOf("2,2,2,2") > -1) {
-      socket.emit("winner", 2);
-    }
-  }
+    return 0;
 };
 
 io.on("connection", function(socket) {
-	var newPlayer = {
-    socket: socket,
-		name: "",
-		role: 0 //0: spectator, 1: player 1, 2: player 2
-	};
-	
-	socket.on("start", function() {
-    if(players[0] === undefined) {
-      socket.emit("connected", 1);
-      newPlayer.role = numPlayers;
-      players[0] = newPlayer;
-    }
-    else if(players[1] === undefined) {
-      socket.emit("connected", 2);
-      newPlayer.role = numPlayers;
-      players[1] = newPlayer;
-    }
-    else {
-      socket.emit("connected", 0);
-      spectators[socket.id] = newPlayer;
-    }
-    socket.emit("newBoard", boardState);
-  });
-  
-  socket.on("placePiece", function(col) {
-    if(newPlayer.role === 1 || newPlayer.role === 2) {
-      if(newPlayer.role !== currTurn) {
-        socket.emit("errorMsg", "It is not your turn yet");
-      }
-      else if(col < 0 || col > boardState.length - 1 || boardState[col].length >= maxHeight) {
-        socket.emit("errorMsg", "Invalid piece placement");
-      }
-      else {
-        boardState[col].push(newPlayer.role);
+    var newPlayer = {
+        socket: socket,
+        name: "",
+        role: 0 //0: spectator, 1: player 1, 2: player 2
+    };
+
+    socket.on("start", function() {
+        if(players[0] === undefined) {
+            socket.emit("connected", 1);
+            newPlayer.role = 1;
+            players[0] = newPlayer;
+        }
+        else if(players[1] === undefined) {
+            socket.emit("connected", 2);
+            newPlayer.role = 2;
+            players[1] = newPlayer;
+        }
+        else {
+            socket.emit("connected", 0);
+            spectators[socket.id] = newPlayer;
+        }
+        console.log("Player joined as " + newPlayer.role);
         socket.emit("newBoard", boardState);
-        checkWin();
-        
-        currTurn = (currTurn === 1) ? 2 : 1;
-      }
-    }
-    else {
-      // player is spectator
-      socket.emit("errorMsg", "Player is spectator, cannot place pieces");
-    }
-  });
+
+        if(players[0] && players[1]) {
+            players[0].socket.emit("turn");
+            currTurn = 1;
+        }
+    });
+
+    socket.on("placePiece", function(col) {
+        console.log("Placing piece as " + newPlayer.role + " at " + col);
+        if(newPlayer.role === 1 || newPlayer.role === 2) {
+            if(newPlayer.role !== currTurn) {
+                socket.emit("errorMsg", "It is not your turn yet");
+            }
+            else if(col < 0 || col > boardState.length - 1 || boardState[col].length >= maxHeight) {
+                socket.emit("errorMsg", "Invalid piece placement");
+            }
+            else {
+                console.log("pushed");
+                boardState[col].push(newPlayer.role);
+                console.log(boardState);
+
+                players[0].socket.emit("newBoard", boardState);
+                players[1].socket.emit("newBoard", boardState);
+                for(var id in spectators) {
+                    spectators[id].socket.emit("newBoard", boardState);
+                }
+
+                var win = checkWin();
+                if(win === 1) {
+                    players[0].socket.emit("winner");
+                }
+                else if(win === 2) {
+                    players[1].socket.emit("winner");
+                }
+                else if(currTurn === 1) {
+                    currTurn = 2;
+                    players[1].socket.emit("turn");
+                }
+                else {
+                    currTurn = 1;
+                    players[0].socket.emit("turn");
+                }
+            }
+        }
+        else {
+            // player is spectator
+            socket.emit("errorMsg", "Player is spectator, cannot place pieces");
+        }
+    });
 
 
-	socket.on('disconnect', function() {
-    if(newPlayer.role === 1) {
-      delete players[0];
-    }
-    else if(newPlayer.role === 2) {
-      delete players[1];
-    }
-    else {
-      delete spectators[socket.id];
-    }
-	});
+    socket.on('disconnect', function() {
+        if(newPlayer.role === 1) {
+            delete players[0];
+        }
+        else if(newPlayer.role === 2) {
+            delete players[1];
+        }
+        else {
+            delete spectators[socket.id];
+        }
+    });
 });
 
 app.use(comp());
-app.use(express.static(__dirname + '/../client'));
+app.use(express.static(__dirname + '/client'));
 var port = process.env.PORT  || 5000;
 http.listen(port, function() {
-	console.log("listening on:" + port);
+    console.log("listening on:" + port);
 });
-
